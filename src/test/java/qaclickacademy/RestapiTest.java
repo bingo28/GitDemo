05e7afb8477549fcf63f806e1bbf32b03b2fdaa9
Ankur Jain <jain.ankur28@gmail.com>
2020-09-22 23:01:32 +1000

    first commit

---
+++ src/test/java/qaclickacademy/RestapiTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestapiTest {
+
+	@Test
+	public void mobile() {
+		System.out.println("mobile");
+	}
+	
+	@Test
+	public void laptop() {
+		System.out.println("laptop");
+	}
+}
